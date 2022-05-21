d57ca8ab7aedde614fbb161a6ef27065b8499ff3
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-21 16:12:18 -0400

    e

--- mood_music.py
+++ mood_music.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
+data = pd.read_csv('training_cases.csv')
+print(data)
+
+X = data.drop(columns=['mood'])
+y = data['mood']
