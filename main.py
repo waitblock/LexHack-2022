d136ac4ec2da559eb684087596c4fb5dbd0cfbcb
EL1212 <105900774+EL1212@users.noreply.github.com>
2022-05-21 16:33:09 -0400

    timer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import smtplib as smtp
+import math
+import time
 
 with open('goals.txt', 'r+') as goal_file:
     goals = goal_file.readlines()
@@ -13,7 +15,46 @@ with open('goals.txt', 'r+') as goal_file:
     print(goal_names)
 
 def pomodoro(goal_times, goal_names):
-    print("placeholder")
+    current_goal_time = goal_times[0]
+    print("Please get rid of any distractions")
+    timer = 5*60
+    while timer > 0:
+            m = timer // 60
+            s = timer % 60
+            print('{:02d}:{:02d}'.format(m, s))
+            time.sleep(1)
+            timer -= 1
+    
+    print("Begin your work")
+    for i in range(current_goal_time // 25):
+        timer = 25*60
+        while timer > 0:
+            m = timer // 60
+            s = timer % 60
+            print('{:02d}:{:02d}'.format(m, s))
+            time.sleep(1)
+            timer -= 1
+        print("Time to take a break")
+
+        timer = 5*60
+        while timer > 0:
+            m = timer // 60
+            s = timer % 60
+            print('{:02d}:{:02d}'.format(m, s))
+            time.sleep(1)
+            timer -= 1
+        print("Get back to work")
+
+    print("Time to review your work")
+    timer = 5*60
+    while timer > 0:
+            m = timer // 60
+            s = timer % 60
+            print('{:02d}:{:02d}'.format(m, s))
+            time.sleep(1)
+            timer -= 1
+
+    goal_times.remove(0)
 
 def email_setup():
     print("placeholder")
