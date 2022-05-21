2eca1b638fd8c2d1b17383a7761c75e80da318e1
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-21 16:14:42 -0400

    pep8

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def goToMainScreen():
     main_screen.title('Phokus')
     main_screen.geometry('800x600')
     main_screen.resizeable(False, False)
-    
+
     main_screen.mainloop()
 
 
@@ -24,20 +24,25 @@ def main():
     root.title('Phokus--Login')
     root.geometry('300x200')
     root.resizable(False, False)
+    
     # Title
     title = tk.Label(root, text="Login")
-    title.config(font=('Helvetica bold',30))
+    title.config(font=('Helvetica bold', 30))
     title.grid(row=0, column=0)
+    
     # Get email
     emailLabel = tk.Label(root, text="Email").grid(row=1, column=0)
     global email
     email = tk.StringVar()
     emailEntry = tk.Entry(root, textvariable=email).grid(row=1, column=1)
+    
     # Get password
     passwordLabel = tk.Label(root, text="Password").grid(row=2, column=0)
     global password
     password = tk.StringVar()
-    passwordEntry = tk.Entry(root, textvariable=password, show="*").grid(row=2, column=1)
-    loginButton = tk.Button(root, text="Login", command=validateLogin).grid(row=4, column=0)
+    passwordEntry = tk.Entry(root, textvariable=password,
+                             show="*").grid(row=2, column=1)
+    loginButton = tk.Button(
+        root, text="Login", command=validateLogin).grid(row=4, column=0)
 
     root.mainloop()
