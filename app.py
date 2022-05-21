6642b254b46f95b34f481e4aa890f843606690a3
Ethan <65051086+waitblock@users.noreply.github.com>
2022-05-21 15:52:23 -0400

    login

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+
+
+def validateLogin():
+    with open("credentials.login", "w") as credentials_file:
+        credentials_file.write(email.get())
+        credentials_file.write(password.get())
+    return
+
+
+def main():
+    root = tk.Tk()
+    root.title('App Name')
+    root.geometry('300x200')
+    root.resizable(False, False)
+    # Title
+    title = tk.Label(root, text="Login")
+    title.config(font=('Helvetica bold',30))
+    title.grid(row=0, column=0)
+    # Get email
+    emailLabel = tk.Label(root, text="Email").grid(row=1, column=0)
+    global email
+    email = tk.StringVar()
+    emailEntry = tk.Entry(root, textvariable=email).grid(row=1, column=1)
+    # Get password
+    passwordLabel = tk.Label(root, text="Password").grid(row=2, column=0)
+    global password
+    password = tk.StringVar()
+    passwordEntry = tk.Entry(root, textvariable=password, show="*").grid(row=2, column=1)
+    loginButton = tk.Button(root, text="Login", command=validateLogin).grid(row=4, column=0)
+
+    root.mainloop()
