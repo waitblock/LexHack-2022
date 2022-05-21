b7a530fe4e05c0277ab64fa1ac10549f82ccdc23
EZLiang <47573436+EZLiang@users.noreply.github.com>
2022-05-21 19:45:49 -0400

    yes

--- actual_stuff.py
+++ actual_stuff.py
@@ -3,4 +3,4 @@ import communication, goals
 def mainwindow(update, email, password, main_screen, buddy_button, pomodoro_button, music_button, meditation_button, prnt, showtimer):
     #sendmail = communication.do(email, password)
     goals.pomodoro(["a", "b", "c", "d", "e", "f", "g"], o1=prnt, o2=showtimer, update=update) #TODO:someone figure out why goals.pomodoro is crashing tkinter
-    ...
+    main_screen.mainloop()
