50c0e1ecc54374b497cfae527e1eb944b5af6768
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-21 19:11:44 -0400

    email formatting fix and adding pickling soon

--- communication.py
+++ communication.py
@@ -1,7 +1,21 @@
 import smtplib
 import ssl
+import pickle
 
-port = 465 
+with open('goals.txt', 'r+') as goal_file:
+    goals = goal_file.readlines()
+    goal_names = [line.partition('[')[0].replace('\n', '') for line in goals]
+    goal_times = [line.partition('[')[2].replace(
+        ']', '').replace('\n', '') for line in goals]
+
+    for goal in goal_names:
+        if goal[-1] == ' ':
+            goal_names[goal_names.index(goal)] = goal[:-1]
+
+    goal_names[-1] = 'and ' + goal_names[-1]
+    goal_times[-1] = 'and ' + goal_times[-1]
+
+port = 465
 context = ssl.create_default_context()
 
 login_details = open('credentials.login', 'r')
@@ -9,7 +23,8 @@ users = open('users.txt', 'r')
 users_emails = users.readlines()
 
 for user in users_emails:
-    user.replace('\n', '')
+    users_emails[users_emails.index(user)] = user.replace('\n', '')
+    print(user)
 
 login_lines = login_details.readlines()
 email_username = login_lines[0]
@@ -18,15 +33,13 @@ email_password = login_lines[1]
 message = f"""\
 Subject: Procrastination Accountability Buddy
 
-{email_username.partition('@')[0]} is looking for an accountability buddy to do their homework with"""
+{email_username.partition('@')[0]} is looking for an accountability buddy to do their homework with. Their goals are {', '.join(goal_names)} and they want to spend {' minutes, '.join(goal_times)} minutes on those tasks respectively"""
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     try:
         server.login(email_username, email_password)
-        
-        for user in users:
-            server.sendmail(email_username, users_emails, message)
+        print(users_emails)
+        server.sendmail(email_username, users_emails, message)
         print("Sent")
     except Exception as e:
         print(e)
-    
