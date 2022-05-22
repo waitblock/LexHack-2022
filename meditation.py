7423bd2ea6f44bc6ea2233351a0fc06b561a089f
EZLiang <47573436+EZLiang@users.noreply.github.com>
2022-05-21 21:26:46 -0400

    yes

--- meditation.py
+++ meditation.py
@@ -2,15 +2,17 @@ import tkinter as tk
 import time
 from math import cos
 
-SIZE = 150
+XSIZE = 800
+YSIZE = 600
 
 def meditate(root):
-    meditator = tk.Canvas(root, width=SIZE, height=SIZE, bg="#0080ff", bd=0)
-    meditator.grid(row=3, column=20)
-    circle = meditator.create_oval(0, 0, SIZE, SIZE, fill="white")
+    meditator = tk.Canvas(root, width=XSIZE, height=YSIZE, bg="#0080ff")
+    meditator.place(x=0,y=0)
+    circle = meditator.create_oval(0, 0, YSIZE, YSIZE, fill="white")
     tm = 0
-    while tm < 5:
+    while tm < 2*60:
         time.sleep(0.05)
         tm += 0.05
         root.update()
-        meditator.coords(circle, SIZE*(.2+.2*cos(tm)), SIZE*(.2+.2*cos(tm)), SIZE*(.8-.2*cos(tm)), SIZE*(.8-.2*cos(tm)))
+        meditator.coords(circle, 100+YSIZE*(.2+.1*cos(tm)), YSIZE*(.2+.1*cos(tm)), 100+YSIZE*(.8-.1*cos(tm)), YSIZE*(.8-.1*cos(tm)))
+    meditator.destroy()
