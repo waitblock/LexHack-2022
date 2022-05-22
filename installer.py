401e81197ff0e2393a9af2d9f829f4d9f6d40ef8
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-22 13:59:42 -0400

    eeee

--- installer.py
+++ installer.py
@@ -1,11 +1,19 @@
 import subprocess
 import sys
+from pip._internal.operations import freeze
 
 def install():
     with open('requirements.txt', 'r') as req:
         requirements = req.readlines()
+    
+    # installed = freeze.freeze()
 
     for r in requirements:
-        requirements[requirements.index(r)] = r.replace('\n', '')
-
         subprocess.check_call([sys.executable, "-m", "pip", "install", r])
+
+        # for i in installed:
+        #     if r in i:
+        #         pass
+        #     else:
+        #         requirements[requirements.index(r)] = r.replace('\n', '')
+        #         subprocess.check_call([sys.executable, "-m", "pip", "install", r])
